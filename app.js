416e8bd69c7770ed791302f1a2c8a0fac1b267bb
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-11-15 13:53:14 +0900

    socket is now on https

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+var https = require('https');
+
+var credentials = {
+  key: fs.readFileSync('./cert/file.pem'),
+  cert: fs.readFileSync('./cert/file.crt')
+};
+var serverPort = 3000;
+
+
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/socket.io/node_modules/socket.io-client')));
@@ -8,38 +18,31 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var httpsServer = https.createServer(credentials, app);
+
+var server = httpsServer.listen(serverPort, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
+
 var io = require('socket.io').listen(server);
-
-
 
 (function(){
 
   var self = this;
   self.io_namespace = io.of("/");
-
 	self.io_namespace.on('connection', function(socket){
 		console.log("connected");
-
 	  socket.on('disconnect', function(){
 	    console.log('user disconnected');
 	  });
-
 		socket.on('chat_msg', function(data){
 	    console.log(data);
       socket.emit('test', data.name);
 	  });
-
-
-
-
 	});
-
-
 }());
+
+
