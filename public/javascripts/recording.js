6d59203aac7cb3d0edd22ea96f760708801673b3
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-11-21 15:26:41 +0900

    delete comment

--- public/javascripts/recording.js
+++ public/javascripts/recording.js
@@ -82,24 +82,16 @@ Recording.prototype.start_audio_polling = function(stream){
 
 function convertoFloat32ToInt16(buffer) {
   var len = buffer.length;
-//  var buf = new Int16Array(l);
+
   var double_len = len*2;
   var unit8_buf = new Uint8Array(len);
   var int16_variable = new Int16Array(1);
+
   for (var i=0; i< len; i++) {
-    int16_variable[0] = buffer[i]*0x7FFF;    //convert to 16 bit
-    unit8_buf[2*i] = int16_variable[0] & 0x00FF;
+    int16_variable[0] = buffer[i]*0x7FFF;    //convert to 16 bit PCM
+    unit8_buf[2*i] = int16_variable[0] & 0x00FF; //convert to uint8 for stream buffer
     unit8_buf[2*i+1] = (int16_variable[0] & 0xFF00) >> 8;
   }
 
-/*
-  for(var i=0; i<l_original; i++){
-  	unit8_buf[2*i] = (buf[i] & 0x00FF);
-  	unit8_buf[2*i+1] = (buf[i] & 0xFF00) >> 8;
-  }
-*/
   return unit8_buf.buffer
-  
-//  return buf;
-//  return buf.buffer;
 }
